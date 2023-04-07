90ab9637930c23df0b2030d557d56b7b64036619
PaullaGomes <paullagomes332@gmail.com>
2023-04-07 17:37:57 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import pyautogui
+import webbrowser
+
+webbrowser.open('https://cursoautomacao.netlify.app/')
+
+
+
+pyautogui.click(1222,565, duration=2)
+pyautogui.scroll(-2600)
+pyautogui.click(1222,630, duration=2)
+pyautogui.typewrite('Paulla Gomes')
+pyautogui.click(1187,709, duration=2)
+pyautogui.click(1818,288, duration=2)
+pyautogui.scroll(-1800)
+pyautogui.click(1292,356, duration=2)
+pyautogui.click(1315,581,duration=2)
+pyautogui.confirm('TERMINOU!')
+
+
+
+
+
+
+
+
+
